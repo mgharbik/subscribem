c57a1df225d0c266e64000a51f8d4dc51ae47ec5
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-08-31 15:29:22 +0200

    restricting subdomain names

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -1,8 +1,18 @@
 module Subscribem
   class Account < ActiveRecord::Base
+    EXCLUDED_SUBDOMAINS = %w(admin)
+
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
 
     validates :subdomain, :presence => true, :uniqueness => true
+    validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS,
+                           :message => "is not allowed. Please choose another subdomain."
+    validates_format_of :subdomain, :with => /\A[\w\-]+\Z/i,
+                        :message => "is not allowed. Please choose another subdomain."
+
+    before_validation do
+      self.subdomain = subdomain.to_s.downcase
+    end
   end
 end
