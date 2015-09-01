37e1d8aeb58d782d13ec36ef1ce142acc3ba4bfb
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-09-01 11:09:57 +0200

    handling the user signup

--- app/controllers/subscribem/application_controller.rb
+++ app/controllers/subscribem/application_controller.rb
@@ -27,5 +27,10 @@ module Subscribem
       end
     end
 
+    def force_authentication!(account, user)
+      env["warden"].set_user(user, :scope => :user)
+      env["warden"].set_user(account, :scope => :account)
+    end
+
   end
 end
