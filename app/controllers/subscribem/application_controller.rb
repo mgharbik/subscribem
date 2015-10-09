f227c9385675e4b4af92201250e8064455479c2e
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-10-09 17:35:18 +0200

    scoping by a database field

--- app/controllers/subscribem/application_controller.rb
+++ app/controllers/subscribem/application_controller.rb
@@ -1,36 +1,4 @@
 module Subscribem
-  class ApplicationController < ActionController::Base
-
-    def current_account
-      if user_signed_in?
-        @current_account ||= env["warden"].user(:scope => :account)
-      end
-    end
-    helper_method :current_account
-
-    def current_user
-      if user_signed_in?
-        @current_user ||= env["warden"].user(:scope => :user)
-      end
-    end
-    helper_method :current_user
-
-    def user_signed_in?
-      env["warden"].authenticated?(:user)
-    end
-    helper_method :user_signed_in?
-
-    def authenticate_user!
-      unless user_signed_in?
-        flash[:notice] = "Please sign in."
-        redirect_to "/sign_in"
-      end
-    end
-
-    def force_authentication!(account, user)
-      env["warden"].set_user(user, :scope => :user)
-      env["warden"].set_user(account, :scope => :account)
-    end
-
+  class ApplicationController < ::ApplicationController
   end
 end
