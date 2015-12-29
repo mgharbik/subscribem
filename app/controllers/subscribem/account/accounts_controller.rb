9739f128f04de84aab182110566c53e4594642fd
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-12-29 16:35:51 +0100

    restricting account editing to owners only

--- app/controllers/subscribem/account/accounts_controller.rb
+++ app/controllers/subscribem/account/accounts_controller.rb
@@ -2,6 +2,8 @@ require_dependency "subscribem/application_controller"
 
 module Subscribem
   class Account::AccountsController < ApplicationController
+    before_filter :authenticate_user!
+    before_filter :authorize_owner, only: [:edit, :update]
 
     def update
       if current_account.update_attributes(account_params)
@@ -9,7 +11,7 @@ module Subscribem
         redirect_to root_path
       end
     end
-    
+
     private
     def account_params
       params.require(:account).permit(:name)
--- spec/features/accounts/updating_spec.rb
+++ spec/features/accounts/updating_spec.rb
@@ -18,4 +18,16 @@ feature "Accounts" do
       expect(account.reload.name).to eq("A new name")
     end
   end
+
+  context "as a user" do
+    before do
+      user = FactoryGirl.create(:user)
+      sign_in_as(:user => user, :account => account)
+    end
+
+    scenario "cannot edit an account's information" do
+      visit subscribem.edit_account_url(:subdomain => account.subdomain)
+      expect(page).to have_content("You are not allowed to do that.")
+    end
+  end
 end
